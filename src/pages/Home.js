4f4d518d10b36e643c55dbc11b0f49f1b75cac97
miha11-hash <d_mihaela@msn.com>
2023-03-02 23:45:08 +0200

    homepage- services

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,39 @@
 import React from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import { IconBase } from 'react-icons';
 import {Link} from "react-router-dom";
 import home from "../assets/home.jpg";
+import {services} from "../utils/constants";
 
 function Home() {
+  React.useEffect(() => {
+    AOS.init({
+      offset: 200,
+      duration: 900,
+      easing: "ease-in-sine",
+      delay: 100,
+      disable: "mobile",
+      once: false,
+      startEvent: "DOMContentLoaded",
+      throttleDelay: 99,
+      scroll: true
+    });
+  }, []);
+
+
+
   return (
     <div className="home-container section-center">
       <div className="home-header">
-        <div className="welcome">
+          <div className="welcome" data-aos="fade-up">
              <h1>Design Your <br></br>Comfort Zone</h1>
               <p>Welcome to our online store where we create and sell exquisite wooden furniture. Each piece is 
                   crafted with care, ensuring a unique and 
                    durable addition to your home. Browse our collection and discover the perfect item to enhance your space.</p>
               <Link to="/products" className="btn">Shop Now</Link>
         </div>
+      
         <div className="welcome-img">
            <img src={home} alt=""></img>
         </div>
@@ -22,13 +43,31 @@ function Home() {
 
           </div>
 
-          <div className="services">
-
-          </div>
+          <div className="services-container">
+            <article>
+              <h3>Custom Furniture Built <br></br>Only for You</h3>
+              <p>Our skilled craftsmen create unique and personalized furniture pieces 
+              tailored to your style and needs. From design to delivery, we ensure the highest 
+              quality and satisfaction.</p>
+            </article>
+            <div className="services">
+              {services.map((service) => {
+                const{id, icon, image, title, text}=service
+                return <div key={id} className="service" 
+                        data-aos={id === 2 ? "" : id === 1 ? "fade-right" : "fade-left"} 
+                        data-aos-delay={id === 2 ? "" : id === 1 ? "300" : "500"}
+                        >
+                    <div className="icon">{icon}</div>
+                    <h4>{title}</h4>
+                    <p>{text}</p>
+                </div>
+              })}
+            </div>
 
           <div className="newsletter">
 
           </div>
+        </div>
     </div>
   );
 }
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,33 @@
+import React from 'react'
+import { GiCompass, GiDiamondHard, GiStabbedNote } from 'react-icons/gi'
+
+
 export const links=[
     {id:1, text: "Home", url: "/"},
     {id: 2, text: "Products", url: "/products"},
     {id: 3, text: "About", url: "/about"}
 ]
+
+export const services = [
+    {
+      id: 1,
+      icon: <GiCompass />,
+      title: 'mission',
+      text:
+    "We create timeless and functional furniture pieces that reflect the beauty and durability of natural wood. We are committed to using sustainable materials and ethical practices to ensure that our furniture has a minimal impact on the environment. We strive to exceed our customers' expectations by providing personalized service and quality craftsmanship in every piece we create.",
+    },
+    {
+      id: 2,
+      icon: <GiDiamondHard />,
+      title: 'vision',
+      text:
+        "We provide unique and bespoke wooden furniture. We aim to create innovative designs that inspire and reflect the personal style and needs of our clients. We are dedicated to achieving excellence in every aspect of our business, from design to delivery, and we aim to build long-term relationships with our customers based on trust, quality, and reliability.",
+    },
+    {
+      id: 3,
+      icon: <GiStabbedNote />,
+      title: 'history',
+      text:
+        "Our company has a rich history of creating and selling beautiful and functional wooden furniture. Today, we continue to honor our legacy by using only the finest materials and the same attention to detail that has made us a trusted name in the industry. We are proud of our heritage and remain committed to creating and selling quality furniture that stands the test of time.",
+    },
+  ]
