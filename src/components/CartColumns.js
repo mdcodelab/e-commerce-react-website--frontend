5124bfc99692172818d7d70f83a1ac4efa5cc629
miha11-hash <d_mihaela@msn.com>
2023-03-30 12:24:57 +0300

    cart totals

--- src/components/CartColumns.js
+++ src/components/CartColumns.js
@@ -2,8 +2,14 @@ import React from 'react';
 
 function CartColumns() {
   return (
-    <div>
-      
+    <div className="cartColumns-container">
+      <div className="content">
+      <h5>Item</h5>
+      <h5>Price</h5>
+      <h5>Quantity</h5>
+      <h5>Subtotal</h5>
+    </div>
+    <hr></hr>
     </div>
   );
 }
