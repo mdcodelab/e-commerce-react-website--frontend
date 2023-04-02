2b99f7e7f39dc33f8af4e96e3952c2cf732e9d4f
miha11-hash <d_mihaela@msn.com>
2023-04-02 17:28:08 +0300

    auth0 setup

--- src/index.js
+++ src/index.js
@@ -5,16 +5,26 @@ import App from './App';
 import { ProductsProvider } from './context/products_context';
 import { FilterProvider } from './context/filter_context';
 import { CartProvider} from './context/cart_context';
+import { Auth0Provider } from "@auth0/auth0-react";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <ProductsProvider>
+  <Auth0Provider
+  domain={process.env.REACT_APP_AUTH0_DOMAIN}
+    clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
+    authorizationParams={{
+      redirect_uri: window.location.origin}}
+      cachesLocation="LocalStorage"
+  >
+    <ProductsProvider>
     <FilterProvider>
     <CartProvider>
       <App></App>
     </CartProvider>
     </FilterProvider>
   </ProductsProvider>
+  </Auth0Provider>
+  
 );
 
