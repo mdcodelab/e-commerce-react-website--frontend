a8c166a7aaf59e137284412be9d1abef0bbb333e
miha11-hash <d_mihaela@msn.com>
2023-04-07 23:17:00 +0300

    Stripe Checkout basic return

--- src/components/StripeCheckout.js
+++ src/components/StripeCheckout.js
@@ -1,42 +1,78 @@
 import React from 'react';
-import {loadStripe} from '@stripe/stripe-js';
-import {PaymentElement, LinkAuthenticationElement, useStripe, useElements} from "@stripe/react-stripe-js";
-import axios from "axios";
+import { loadStripe } from '@stripe/stripe-js';
+import {
+  CardElement,
+  Elements,
+  useStripe,
+  useElements
+} from '@stripe/react-stripe-js';
+import axios from 'axios';
 import { useCartContext } from '../context/cart_context';
 import { useUserContext } from '../context/user_context';
 import { formatPrice } from '../utils/helpers';
-import {useHistory} from "react-router-dom";
+//import { useHistory } from 'react-router-dom';
+
+
 
 const promise = loadStripe(process.env.REACT_APP_STRIPE_PUBLIC);
-console.log(promise);
+
 
 
 const CheckoutForm = () => {
-  const {cart, total_amount, shipping_fee, clearCart}=useCartContext();
-  const {myYser}=useUserContext();
-  const history=React.useHistory();
-  //stripe stuff
-  const[succeeded, setSucceeded]=React.useState(false);
-  const[error, setError]=React.useState(null);
-  const[processing, setProcessing]=React.useState("");
-  const[disabled, setDisabled]=React.useState(true);
-  const[clientSecret, setClientSecret]=React.useState("");
+  const { cart, total_amount, shipping_fee, clearCart } = useCartContext();
+  const { myUser } = useUserContext();
+  //const history = useHistory();
+  
+  // stripe stuff
+  const [succeeded, setSucceeded] = React.useState(true);
+  const [error, setError] = React.useState(null);
+  const [processing, setProcessing] = React.useState("");
+  const [disabled, setDisabled] = React.useState(true);
+  const [clientSecret, setClientSecret] = React.useState("");
   const stripe = useStripe();
   const elements = useElements();
 
+  const createPaymentIntent = async () => {
+    console.log("hello from stripe checkout");
+  };
 
-return <h2>Hello from Stripe Checkout</h2>
-}
+  React.useEffect(() => {
+    createPaymentIntent();
+  }, []);
+
+  const handleChange = (event) => {};
+
+  const handleSubmit = (ev) => {};
+
+  return (
+    <div className="section">
+      <form id="payment-form" onSubmit={handleSubmit}>
+        <CardElement id="link-authentication-element" onChange={handleChange} />
+        <button className="bt" disabled={processing || disabled || succeeded}>
+        <span>{processing ? <div className="spinner-check"></div> : "PAY"}</span></button>
+
+        {/* Show any error that happens when processing the payment*/}
+        {error && <div className="card-error" rile="alert">{error}</div>}
+
+        {/* Show a success message upon completion*/}
+        <p className={succeeded ? "result-message" : "result-message hidden"}>
+        Payment succeeded, see the result in your <a href={`https://dashboard.stripe.com/test/elements`}>
+        Stripe dashboard. </a>
+        <span>Refresh the page to try again</span>
+        </p>
+      </form>
+    </div>
+  );
+};
 
 const StripeCheckout = () => {
   return (
     <div className="stripe-container">
-    <PaymentElement stripe={promise}>
-        <CheckoutForm></CheckoutForm>
-    </PaymentElement>
-      
+      <Elements stripe={promise}>
+        <CheckoutForm />
+      </Elements>
     </div>
-  )
-}
+  );
+};
 
 export default StripeCheckout;
