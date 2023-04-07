60a3413de28c9e264197119205353783ae521025
miha11-hash <d_mihaela@msn.com>
2023-04-07 14:31:02 +0300

    StripeCheckout imports

--- src/components/StripeCheckout.js
+++ src/components/StripeCheckout.js
@@ -1,4 +1,15 @@
 import React from 'react';
+import {loadStripe} from '@stripe/stripe-js';
+import {PaymentElement, LinkAuthenticationElement, useStripe, useElements} from "@stripe/react-stripe-js";
+import axios from "axios";
+import { useCartContext } from '../context/cart_context';
+import { useUserContext } from '../context/user_context';
+import { formatPrice } from '../utils/helpers';
+import {useHistory} from "react-router-dom";
+
+const promise = loadStripe(process.env.REACT_APP_STRIPE_PUBLIC);
+console.log(promise);
+
 
 const CheckoutForm = () => {
 return <h2>Hello from Stripe Checkout</h2>
@@ -7,7 +18,10 @@ return <h2>Hello from Stripe Checkout</h2>
 const StripeCheckout = () => {
   return (
     <div className="stripe-container">
-      <CheckoutForm></CheckoutForm>
+    <PaymentElement stripe={promise}>
+        <CheckoutForm></CheckoutForm>
+    </PaymentElement>
+      
     </div>
   )
 }
